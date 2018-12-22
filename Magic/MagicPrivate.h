0930e695de18850596e6d2f3d8aaec13bb288c3f
Kishikawa Katsumi <kishikawakatsumi@mac.com>
2018-12-23 02:27:58 +0900

    Add MagicPrivate module

---
+++ Magic/MagicPrivate.h
@@ -0,0 +1,17 @@
+//
+//  MagicPrivate.h
+//  Magic
+//
+//  Created by Kishikawa Katsumi on 2018/12/23.
+//  Copyright © 2018 Kishikawa Katsumi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MagicPrivate : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
